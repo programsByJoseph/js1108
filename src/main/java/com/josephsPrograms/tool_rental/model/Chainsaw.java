ac1bf012031c5da242a345bab0e4f0ee67525989
Joseph Starr <josephwstarr@gmail.com>
2025-11-08 12:50:07 -0800

    abstract tool class and individual tool classes

---
+++ src/main/java/com/josephsPrograms/tool_rental/model/Chainsaw.java
@@ -0,0 +1,17 @@
+package com.josephsPrograms.tool_rental.model;
+
+import java.math.BigDecimal;
+
+public class Chainsaw extends Tool {
+    public Chainsaw(String toolCode, String brand) {
+        super(
+                toolCode,
+                "Chainsaw",
+                brand,
+                BigDecimal.valueOf(1.49),
+                true,
+                false,
+                true
+        );
+    }
+}
